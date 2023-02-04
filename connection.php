2bf49526f38ea3a9f102ca82822c7d892732bbf1
Krishna6802 <118924652+Krishna6802@users.noreply.github.com>
2023-02-04 07:39:01 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head> 
+   <title>Connection Page</title>  
+</head>
+
+<?php
+session_start();
+$con = mysqli_connect("localhost","root","");
+$db = mysqli_select_db($con,"db_ecare");
+
+date_default_timezone_set('Asia/Kolkata');
+?>
+
