a6f3d50330f89c42dd2c17a00f5080249fe6fa0b
Joshi Krishna <118924652+Krishna6802@users.noreply.github.com>
2023-07-20 15:46:04 +0530

    Delete admin directory

--- admin/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$con = mysqli_connect("localhost","root","");
-$db = mysqli_select_db($con,"db_ecare");
-
-date_default_timezone_set('Asia/Kolkata');
-?>
-
--- admin/footer.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-?>
-
-<footer>
-</footer>
